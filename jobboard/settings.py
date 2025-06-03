cfdbeb77216a57025ffa32fb01a27aa8c67c8ddb
vsteve96 <vizhanyo.istvan0@gmail.com>
2025-06-03 17:56:23 +0200

    prepare project for Windows IDE environment instead of Gitpod/Linux

--- jobboard/settings.py
+++ jobboard/settings.py
@@ -16,17 +16,13 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if os.path.exists("env.py"):
-    import env
-    
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
