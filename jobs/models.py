28aa2ff4c1e6b210bf9944a42f7a86a007524477
vsteve96 <vizhanyo.istvan0@gmail.com>
2025-06-04 15:30:45 +0200

    Imports the Post model and registers it, renames description field to content

--- jobs/models.py
+++ jobs/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User # Import models to connect
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 class Post(models.Model):
-## Create a class named Post inheriting from the Model class
+# Represents a job post created by a user (typically a freelancer).
     title = models.CharField(max_length=200, unique=True)
-    description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
